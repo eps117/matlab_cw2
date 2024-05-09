c31bca77e3c719d9a0936184108b06a927947d7a
eps117 <167983173+eps117@users.noreply.github.com>
2024-05-09 13:35:09 +0100

    Add files via upload

---
+++ TASK_4_CW2.m
@@ -0,0 +1,18 @@
+% TASK 4: REFLECTIVE STATEMENT [5 MARKS]
+
+% One of the challenges I faced this courework was in task 2, where I
+% could not get the LEDs to turn on whilst the programme was running. I
+% also was not able to get my live plot working fully as it was not showing
+% the plot. Another challenge I faced was in both task 2 and 3, as I was
+% unable to create a function for temp_prediction and temp_monitor as
+% different amber wanrings would appear saying different variables were unused.
+% However, one strength I had was task 1 in general, as printing text to
+% the screen using fprintf was previously learnt in the last coursework, so
+% I could call upon my previous knowledge.
+% A limitation I experienced was my lack of understanding when it came to
+% running a programme for an indefinite time, as that requires 'while true'
+% statements and infinite loops which is not a strong point of mine
+% If I was to repeat this coursework, I would improve on my knowledge of
+% infinite loops and while true statements. In addition to this I would
+% try and understand how to live plot graphs to show the data from the code
+% whilst it is running indefinitely.
